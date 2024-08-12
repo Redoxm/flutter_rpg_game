75e5cc17dbd8e7e08dedbecfe87b9d3de4250b27
Luqman adebayo <luqmanaremu4@gmail.com>
2024-08-12 15:27:36 +0100

    rpg

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masterclass/screens/home/home.dart';
+import 'package:flutter_masterclass/services/character_store.dart';
+import 'package:flutter_masterclass/theme.dart';
+import 'package:provider/provider.dart';
+
+
+// initialize firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
+
+  runApp(ChangeNotifierProvider(
+    create: (context) => CharacterStore(),
+    child: MaterialApp(
+      theme: primaryTheme,
+      home: const Home(),
+    ),
+  ));
+}
+
+// sandbox
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('sandbox'),
+    );
+  }
+}
